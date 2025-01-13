d5086740540906144f10f270113c6089cb3f0b2b
Narjes <151176138+Molapour80@users.noreply.github.com>
2025-01-13 20:46:12 +0330

    Update Create Folder.py

--- Create Folder.py
+++ Create Folder.py
@@ -1,6 +1,6 @@
 import os
 directory = "E:\100Days_python"
-for i in range(13, 101):
+for i in range(0, 101):
     folder_name = f"DAY_{i}"
     os.makedirs(os.path.join(directory, folder_name), exist_ok=True)
 
